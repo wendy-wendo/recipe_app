225ddd21013534d5363a54913a599e16e711c1e1
wendywendo <wendywendo06@gmail.com>
2024-05-03 18:12:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route index element={<Main 
